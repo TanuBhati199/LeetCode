29f45742d6bd6e794a604ed0e3198ec3d0720cff
Tanu <146454637+TanuBhati199@users.noreply.github.com>
2024-06-02 18:15:28 +0530

    Time: 19 ms (47.68%), Space: 26.9 MB (60.22%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -8,9 +8,10 @@ public:
       s=ns;*/
       int i=0,j=s.size()-1;
       while(i<j){
-        int temp=s[i];
+     /*   int temp=s[i];
         s[i]=s[j];
-        s[j]=temp;
+        s[j]=temp;*/
+        swap(s[i],s[j]);
         i++;
         j--;
       } 
